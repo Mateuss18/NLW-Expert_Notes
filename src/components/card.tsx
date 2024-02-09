e96d1082020796d44e4738ab8802b211bc156eda
Mateus Lopes <mateus20.lopes02@gmail.com>
2024-02-09 20:13:00 -0300

    Aula 2

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,32 +1,46 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from "date-fns"; 
+import { ptBR } from "date-fns/locale";
+import { X } from "lucide-react";
 
 interface CardProps {
   note: {
-    date: Date
-    content: string
-  }
+    date: Date;
+    content: string;
+  };
 }
 
-export function Card({note}: CardProps) {
+export function Card({ note }: CardProps) {
   return (
     <Dialog.Root>
-        <Dialog.Trigger className="rounded-md bg-red-900 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-red-600 text-left focus-visible:ring-2 focus-visible:ring-lime-400 flex-col">
-          <span className="text-sm font-medium text-red-400">
-            {note.date.toISOString()}
-          </span>
-          <p className="text-sm leading-6 text-red-300">
-            {note.content}
-          </p>
+      <Dialog.Trigger className="rounded-md bg-red-900 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-red-600 text-left focus-visible:ring-2 focus-visible:ring-lime-400 flex-col">
+        <span className="text-sm font-medium text-red-400">
+          {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+        </span>
+        <p className="text-sm leading-6 text-red-300">{note.content}</p>
 
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
-        </Dialog.Trigger>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+      </Dialog.Trigger>
 
-        <Dialog.Portal>
-          <Dialog.Overlay className="inset-0 fixed bg-black/60"/>
-          <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-            Oi
-          </Dialog.Content>
-        </Dialog.Portal>
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/60" />
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full bg-red-700 flex flex-col outline-none h-[60vh]">
+          <Dialog.Close className="absolute right-0 top-0 bg-red-800 p1.5 text-red-400 hover:text-red-100">
+            <X className="size-7" />
+          </Dialog.Close>
+
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-red-400">
+            {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+            </span>
+            <p className="text-sm leading-6 text-red-300">{note.content}</p>
+          </div>
+
+          <button className="w-full bg-red-800 py-4 text-center text-sm text-red-200 outline-none font-medium group">
+            Deseja <span className="text-blue-300 group-hover:underline">apagar está nota</span>?
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
     </Dialog.Root>
   );
 }
--- src/components/new-card.tsx
+++ src/components/new-card.tsx
@@ -1,13 +1,69 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from "sonner";
+
 export function NewCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false)
+  }
+
+  function handleContentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+      if (event.target.value === '') {
+       setShouldShowOnboarding(true) 
+      }
+  }
+
+  function handleSaveNote(event: FormEvent) {
+    event.preventDefault()
+
+    toast.success('Nota criado com sucesso!')
+  }
+
   return (
-    <div className="rounded-md bg-red-800 p-5 space-y-3 overflow-hidden ">
-      <span className="text-sm font-medium text-red-200">Adicionar nota</span>
-      <p className="text-sm leading-6 text-red-300 overflow-hidden">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure
-        perferendis placeat porro, maxime mollitia quidem fugiat deleniti est
-        corrupti doloremque quam quos id culpa, voluptas quae quod quasi.
-        Molestias, laudantium.
-      </p>
-    </div>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md flex flex-col bg-red-800 p-5 gap-3 text-left outline-none hover:ring-2 hover:ring-red-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+          <span className="text-sm font-medium text-red-200">
+            Adicionar nota
+          </span>
+          <p className="text-sm leading-6 text-red-300 overflow-hidden">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure
+            perferendis placeat porro, maxime mollitia quidem fugiat deleniti
+            est corrupti doloremque quam quos id culpa, voluptas quae quod
+            quasi. Molestias, laudantium.
+          </p>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/60" />
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full bg-red-700 flex flex-col outline-none h-[60vh]">
+          <Dialog.Close className="absolute right-0 top-0 bg-red-800 p1.5 text-red-400 hover:text-red-100">
+            <X className="size-7" />
+          </Dialog.Close>
+          
+          <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              <span className="text-sm font-medium text-red-400">
+                Adicionar nota
+              </span>
+
+              {shouldShowOnboarding ? (
+                <p className="text-sm leading-6 text-red-300">
+                Comece <button className="text-green-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor}  className="text-green-400 hover:underline">utilize apenas texto</button>
+                </p>
+              ) : (
+                <textarea autoFocus className="text-sm leading-6 text-red-400 bg-transparent resize-none flex-1 outline-none" onChange={handleContentChange}></textarea>
+              )}
+            </div>
+
+            <button type="submit" className="w-full bg-green-700 py-4 text-center text-sm text-green-950 outline-none font-medium hover:bg-green-400">
+              Salvar nota
+            </button>
+          </form>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
